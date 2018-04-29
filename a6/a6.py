958b9d14ba5467aacee8ad031e16a586df578a6f
David White <dwhite54@colostate.edu>
2018-04-29 12:53:26 -0600

    making some progress, started working in a6.py (just a copy of mnist_with_summaries.py) currently

---
+++ a6/a6.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+FLAGS = None
+
+def train():
+    # Import data
+    mnist = input_data.read_data_sets(FLAGS.data_dir, fake_data=FLAGS.fake_data)
+    sess = tf.InteractiveSession()
+    # Create a multilayer model.
+
+    # Input placeholders
+    with tf.name_scope('input'):
+        x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+        y_ = tf.placeholder(tf.int64, [None], name='y-input')
+
+    with tf.name_scope('input_reshape'):
+        image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
+        tf.summary.image('input', image_shaped_input, 10)
+
+    conv1 = tf.layers.conv2d(
+            inputs=input_layer,
+            filters=32,
+            kernel_size=[5, 5],
+            padding="same",
+            activation=tf.nn.relu)
